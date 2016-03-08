bc166816637df77d95ea8a325b657e2715660358
Pierre Robillard <pierre.j.robillard@gmail.com>
2016-03-08 13:31:28 +0100

    BenchTest OpenCV for Orbox

--- Orbox-Run1/src/application/Main.java
+++ Orbox-Run1/src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 	
+import org.opencv.core.Core;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -11,9 +13,11 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("LocalFXtest.fxml"));
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("View.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setMaximized(true);
+			primaryStage.setTitle("OrBox - CVbenchtest");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
@@ -22,6 +26,7 @@ public class Main extends Application {
 	}
 	
 	public static void main(String[] args) {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		launch(args);
 	}
 }
